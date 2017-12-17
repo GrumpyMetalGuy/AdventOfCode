b97fb7b728e1859ae22eed6923e020a288947d43
Peter Keyzers <peter.keyzers@gmail.com>
2017-12-17 17:42:47 +0000

    Day 4 solution

---
+++ Python/day4.py
@@ -0,0 +1,35 @@
+def loadPassphrases():
+    phrases = []
+
+    with open('day4_input.txt', 'r') as phraseFile:
+        line = phraseFile.readline()
+
+        while line:
+            phrases.append(line[:-1].split(' '))
+            line = phraseFile.readline()
+
+    return phrases
+
+
+def validPhraseCount(phrases):
+    filteredPhrases = [set(phrase) for phrase in phrases if len(phrase) == len(set(phrase))]
+
+    return len(filteredPhrases)
+
+
+def validNonAnagramPhraseCount(phrases):
+    sortedPhrases = [[''.join(sorted(word)) for word in phrase] for phrase in phrases]
+    filteredPhrases = [phrase for phrase in sortedPhrases if len(set(phrase)) == len(phrase)]
+
+    return len(filteredPhrases)
+
+
+def main():
+    passphraseInputs = loadPassphrases()
+
+    print(validPhraseCount(passphraseInputs))
+    print(validNonAnagramPhraseCount(passphraseInputs))
+
+
+if __name__ == '__main__':
+    main()
